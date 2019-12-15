e7c63c1966f3c9322e17240b159d4e7186dff8a5
unknown <jettucker96@gmail.com>
2019-12-14 20:47:28 -0600

    card set up with attributes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './components/layout/NavBar';
 import './App.css';
 import Dashboard from './components/layout/Dashboard';
+import Pokemon from './components/pokemon/Pokemon';
+import backgroundImage from './pattern.png';
 
 function App() {
   return (
-    <div className='App'>
+    <Router>
+    <div className='App' style={{background: `url(${backgroundImage})`}}>
       <Navbar />
       <div className='container'>
-        <Dashboard></Dashboard>
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+        <Dashboard />
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 import spinner from '../pokemon/spinner.gif';
 
@@ -7,6 +8,30 @@ const Sprite = styled.img`
   heigth: 5em;
   display: none;
 `;
+
+const Card = styled.div`
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+  -moz-user-select: none;
+  -website-user-select: none;
+  user-select: none;
+  -o-user-select: none;
+`;
+
+const StyledLink = styled(Link)`
+text-decoration: none;
+color: black;
+&:focus,
+&:hover,
+&:visited,
+&:link,
+&:active {
+  text-decoration: none;
+}
+`
 export default class PokemonCard extends Component {
   state = {
     name: '',
@@ -31,7 +56,8 @@ export default class PokemonCard extends Component {
   render() {
     return (
       <div className='col-md-3 col-sm-6 mb-5'>
-        <div className='card'>
+        <StyledLink to={`pokemon/${this.state.pokemonIndex}`}>
+        <Card className='card'>
           <h5 className='card-header'>{this.state.pokemonIndex}</h5>
           {this.state.imageLoading ? (
             <img
@@ -69,7 +95,8 @@ export default class PokemonCard extends Component {
                 .join(' ')}
             </h6>
           </div>
-        </div>
+        </Card>
+        </StyledLink>
       </div>
     );
   }
